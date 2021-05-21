f8235cacf1f31f255d336cf8e0b410e81269e6c6
Felipe Figueiredo <philsf79@gmail.com>
2021-05-21 12:15:19 -0300

    init fixed

--- bin/AE-init.sh
+++ bin/AE-init.sh
@@ -4,6 +4,6 @@ AE=${PWD##*/} # local dir
 
 echo "Initializing repository for $AE"
 git init >/dev/null
-git add .gitignore README.md $AE.Rproj
+git add .gitignore README.md "$AE.Rproj"
 git commit -m "Initial commit" >/dev/null
 
