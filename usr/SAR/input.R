a0190c04ddc0cbf56003924ab15343a3c5848d27
Felipe Figueiredo <philsf79@gmail.com>
2021-08-30 02:43:52 -0300

    best practices: input template improved

--- usr/SAR/input.R
+++ usr/SAR/input.R
@@ -1,19 +1,46 @@
 # setup -------------------------------------------------------------------
+# library(data.table)
+library(tidyverse)
 library(readxl)
-library(data.table)
-# library(tidyverse)
-
+# library(lubridate)
+library(labelled)
 
 # data loading ------------------------------------------------------------
-# raw.data <- fread("dataset/file.csv")
-# raw.data <- readxl::read_excel("dataset/file.xls")
 
-# raw.data <- data.table(raw.data)
+# data.raw <- read_excel("dataset/file.xlsx") %>%
+#   janitor::clean_names()
+
 
 # data cleaning -----------------------------------------------------------
 
-
+# data.raw <- data.raw %>%
+#   filter() %>%
+#   select()
 
 # data wrangling ----------------------------------------------------------
 
+# data.raw <- data.raw %>%
+#   mutate(
+#
+#   )
 
+# labels ------------------------------------------------------------------
+
+# data.raw <- data.raw %>%
+#   set_variable_labels(
+#
+#   )
+
+# analytical dataset ------------------------------------------------------
+
+# analytical <- data.raw %>%
+#   # select analytic variables
+#   select(
+#
+#   )
+#
+# # mockup of analytical dataset for SAP and public SAR
+# analytical_mockup <- tibble( id = c( "1", "2", "3", "...", as.character(nrow(analytical)) ) ) %>%
+#   left_join(analytical %>% head(0), by = "id") %>%
+#   mutate_all(as.character) %>%
+#   replace(is.na(.), "")
