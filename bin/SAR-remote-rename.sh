fb1e1e0ebb63eb7448370fc6539244e1464d30f1
Felipe Figueiredo <philsf79@gmail.com>
2021-08-30 03:47:19 -0300

    fix: last case of quotes, to avoid issues with spaces in filenames

--- bin/SAR-remote-rename.sh
+++ bin/SAR-remote-rename.sh
@@ -11,7 +11,7 @@ REMOTE=$1
 
 REMOTE_OLDPATH=$(git remote get-url "$REMOTE")
 REMOTE_DIR=$(dirname "$REMOTE_OLDPATH")
-REMOTE_NEWPATH=$REMOTE_DIR/$SAR.git
+REMOTE_NEWPATH="$REMOTE_DIR/$SAR.git"
 echo "Renaming remote $REMOTE to $REMOTE_NEWPATH"
 error=$(mv "$REMOTE_OLDPATH" "$REMOTE_NEWPATH" 2>&1) || echo "Can't rename remote path: $error"
 error=$(git remote set-url "$REMOTE" "$REMOTE_NEWPATH" 2>&1) || echo "Fail on git set-url: $error";
