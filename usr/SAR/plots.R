c9773dcce423b3c25e5ce481240891b3feb5f690
Felipe Figueiredo <philsf79@gmail.com>
2021-08-30 03:44:10 -0300

    best practices: plots template improved

--- usr/SAR/plots.R
+++ usr/SAR/plots.R
@@ -2,6 +2,16 @@
 # library(ggplot2)
 # library(survminer)
 
-# plots -------------------------------------------------------------------
+ff.col <- "steelblue" # good for single groups scale fill/color brewer
+ff.pal <- "Paired"    # good for binary groups scale fill/color brewer
 
+# Theme setting (less is more)
+theme_set(
+  theme_classic() +
+    theme_update(
+      legend.position = "top"
+    )
+)
+
+# plots -------------------------------------------------------------------
 
