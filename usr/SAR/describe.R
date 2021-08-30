8cb1f7bab07f756c361f6e4330349db2b91a059e
Felipe Figueiredo <philsf79@gmail.com>
2021-08-30 03:16:57 -0300

    best practices: describe template improved

--- usr/SAR/describe.R
+++ usr/SAR/describe.R
@@ -4,8 +4,22 @@
 # library(skimr) # skim
 # library(tableone)
 # library(gmodels) # CrossTable
+library(gtsummary)
+library(gt)
+library(effectsize)
+
+# setup gtsummary theme
+# theme_gtsummary_mean_sd() # mean/sd
+# theme_gtsummary_language(language = "pt") # traduzir
 
 # exploratory -------------------------------------------------------------
+
+# overall description
+# analytical %>%
+#   skimr::skim()
+
+# minimum detectable effect size
+# interpret_d(0.5)
 
 
 # tables ------------------------------------------------------------------
