d949b9e1963689f4c702715059dfec821a749f88
Felipe Figueiredo <philsf79@gmail.com>
2021-09-03 17:22:17 -0300

    inference: mockup inferential table + disable Cohen's d table

--- usr/SAR/inference.R
+++ usr/SAR/inference.R
@@ -6,28 +6,36 @@
 
 # tables ------------------------------------------------------------------
 
-## template p-value table
+# template p-value table
+tab_inf <- analytical %>%
+  # select
+  select(-id, ) %>%
+  tbl_summary(
+    by = group
+  ) %>%
+  # include study N
+  add_overall() %>%
+  # pretty format categorical variables
+  bold_labels() %>%
+  # bring home the bacon!
+  add_p(
+    # use Fisher test (defaults to chi-square)
+    test = all_categorical() ~ "fisher.test",
+    # use 3 digits in pvalue
+    pvalue_fun = function(x) style_pvalue(x, digits = 3)
+  ) %>%
+  # bold significant p values
+  bold_p()
+
+# Template Cohen's D table (obs: does NOT compute p)
 # tab_inf <- analytical %>%
-#   tbl_summary() %>%
-#   # include study N
-#   add_overall() %>%
-#   # pretty format categorical variables
-#   bold_labels() %>%
-#   # bring home the bacon!
-#   add_p(
-#     # use Fisher test (defaults to chi-square)
-#     test = all_categorical() ~ "fisher.test",
-#     # use 3 digits in pvalue
-#     pvalue_fun = function(x) style_pvalue(x, digits = 3)
+#   # select
+#   select(-id, ) %>%
+#   tbl_summary(
+#     by = group
 #   ) %>%
-#   # bold significant p values
-#   bold_p()
-
-# Template Cohen's D table (obs: only computes p if all variables are continuous)
-# tab_inf <- analytical %>%
-#   tbl_summary() %>%
-#   add_difference(test = all_continuous() ~ "cohens_d") %>%
+#   add_difference(
+#     test = all_continuous() ~ "cohens_d",
+#   ) %>%
 #   modify_header(estimate ~ '**d**') %>%
-#   bold_p() %>%
 #   bold_labels()
-
