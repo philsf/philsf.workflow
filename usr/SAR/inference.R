d831789e961ef049883c2530e1e8f0b63840dfb6
Felipe Figueiredo <philsf79@gmail.com>
2021-11-02 17:19:57 -0300

    new: ANCOVA template

--- usr/SAR/inference.R
+++ usr/SAR/inference.R
@@ -16,13 +16,24 @@ tab_inf <- analytical %>%
   add_overall() %>%
   # pretty format categorical variables
   bold_labels() %>%
-  # bring home the bacon!
+  # bring home the bacon! (options: add_p or add_difference)
+  # add_p: quick and dirty
   add_p(
     # use Fisher test (defaults to chi-square)
     test = all_categorical() ~ "fisher.test",
     # use 3 digits in pvalue
     pvalue_fun = function(x) style_pvalue(x, digits = 3),
   ) %>%
+  # # diff: alternative to add_p
+  #   add_difference(
+  #     test = all_categorical() ~ "fisher.test",
+  #     # use 3 digits in pvalue
+  #     pvalue_fun = function(x) style_pvalue(x, digits = 3),
+  #     # ANCOVA
+  #     # adj.vars = c(sex, age, bmi),
+  #   ) %>%
+  # modify_header(estimate ~ '**Diferença ajustada**') %>%
+  # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (ajusted by sex, age and BMI)") %>%
   # bold significant p values
   bold_p()
 
@@ -37,6 +48,8 @@ tab_inf <- analytical %>%
 #   ) %>%
 #   add_difference(
 #     test = all_continuous() ~ "cohens_d",
+#     # ANCOVA
+#     adj.vars = c(sex, age, bmi),
 #   ) %>%
 #   modify_header(estimate ~ '**d**') %>%
 #   bold_labels()
