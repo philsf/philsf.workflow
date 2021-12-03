747f8a1f72b4c6a3c3d1d3a3859a0027648db1c2
Felipe Figueiredo <philsf79@gmail.com>
2021-12-03 18:59:09 -0300

    sinew generated roxygen

--- R/theme_ff.R
+++ R/theme_ff.R
@@ -1,3 +1,16 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname theme_ff
+#' @export
 theme_ff <- function(...) {
   theme_classic(
     ...,
