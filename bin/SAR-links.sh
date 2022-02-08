5cc984439173db4dfd2c0166c2ad8d1ffe488478
Felipe Figueiredo <philsf79@gmail.com>
2022-02-07 22:43:29 -0300

    SAR-links now prints usage when called with not args

--- bin/SAR-links.sh
+++ bin/SAR-links.sh
@@ -3,4 +3,10 @@
 orig="yyyy-NNN-XX"
 dest=$1
 
+if [[ "$dest" == "" ]]
+then
+    echo "Usage: SAR-links.sh $orig"
+    exit
+fi
+
 sed -i "s/$orig/$dest/g" README.md scripts/* report/*md 2>&1 >/dev/null
