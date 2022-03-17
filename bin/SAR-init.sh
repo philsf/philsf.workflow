ceaeee2556248364a6940cad75d97e7d0bae8b55
Felipe Figueiredo <philsf79@gmail.com>
2022-03-17 20:23:57 -0300

    offer optional language version for the doc templates

--- bin/SAR-init.sh
+++ bin/SAR-init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SAR=${PWD##*/} # local dir
+SAR_CODE=${SAR/SAR-/""}
 
 echo "Initializing repository: $SAR"
 git init >/dev/null
@@ -16,3 +17,9 @@ cp ~/usr/SAR/README-SAR_en.md README.md
 
 echo "Porfolio (en) applied over default (opt-out?)"
 cp ~/usr/SAR/Portfolio_en.md Portfolio.md
+
+echo "Document templates (en) applied over default (opt-out?)"
+cp -f ~/usr/SAR/SAP_en.Rmd report/SAP-$SAR_CODE-v01.Rmd
+cp -f ~/usr/SAR/SAR_en.Rmd report/SAR-$SAR_CODE-v01.Rmd
+cp -a ~/usr/SAR/misc/*en.docx report/misc/
+rm -f report/misc/*pt.docx
