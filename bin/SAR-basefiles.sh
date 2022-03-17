9a9cbabd19a6cc54673293751b28e9009711f989
Felipe Figueiredo <philsf79@gmail.com>
2022-03-17 20:16:44 -0300

    use bash string manipulation to auto-set the SAR code

--- bin/SAR-basefiles.sh
+++ bin/SAR-basefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SAR=${PWD##*/} # local dir
+SAR_CODE=${SAR/SAR-/""}
 
 RSCRIPTS="
 input.R
@@ -26,6 +27,6 @@ cp ~/usr/SAR/README-SAR_pt.md README.md
 cp ~/usr/SAR/Portfolio_pt.md Portfolio.md
 
 # apply default SAP/SAR templates
-cp ~/usr/SAR/SAP_pt.Rmd report/SAP-yyyy-NNN-XX-v01.Rmd
-cp ~/usr/SAR/SAR_pt.Rmd report/SAR-yyyy-NNN-XX-v01.Rmd
+cp ~/usr/SAR/SAP_pt.Rmd report/SAP-$SAR_CODE-v01.Rmd
+cp ~/usr/SAR/SAR_pt.Rmd report/SAR-$SAR_CODE-v01.Rmd
 cp -a ~/usr/SAR/misc/*pt.docx report/misc/
