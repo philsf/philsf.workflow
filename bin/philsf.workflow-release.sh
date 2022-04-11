be1ebd5427cc9bc43b3c2f34e9b280765e50e9cd
Felipe Figueiredo <philsf79@gmail.com>
2022-04-11 19:39:40 -0300

    new script to update doc templates to be installed in new repos

---
+++ bin/philsf.workflow-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Updating templates that will be installed on next release."
+/bin/cp -a inst/rmarkdown/templates/SAP_pt/skeleton/skeleton.Rmd usr/SAR/SAP_pt.Rmd
+/bin/cp -a inst/rmarkdown/templates/SAP_en/skeleton/skeleton.Rmd usr/SAR/SAP_en.Rmd
+/bin/cp -a inst/rmarkdown/templates/SAR_pt/skeleton/skeleton.Rmd usr/SAR/SAR_pt.Rmd
+/bin/cp -a inst/rmarkdown/templates/SAR_en/skeleton/skeleton.Rmd usr/SAR/SAR_en.Rmd
