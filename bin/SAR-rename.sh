5a25cc3c9cd9d9a9255633ac0be88fd14eef9df9
Felipe Figueiredo <philsf79@gmail.com>
2022-04-11 20:01:39 -0300

    rename installed templates (v01 for now)

--- bin/SAR-rename.sh
+++ bin/SAR-rename.sh
@@ -17,6 +17,14 @@ cd "$NEW_SAR" || { echo "Can't enter repository"; exit 1; }
 # rename Rstudio project
 error=$(git mv "$OLD_SAR.Rproj" "$NEW_SAR.Rproj" 2>&1) || echo "Can't rename RStudio Project: $error"
 
+# rename v01 documents
+OLD_SAP_FILE=${OLD_SAR/SAR-/"SAP-"}
+NEW_SAP_FILE=${NEW_SAR/SAR-/"SAP-"}
+
+error=$(git mv "report/$OLD_SAP_FILE-v01.Rmd" "report/$NEW_SAP_FILE-v01.Rmd" 2>&1) || echo "Can't rename SAP v01: $error"
+
+error=$(git mv "report/$OLD_SAR-v01.Rmd" "report/$NEW_SAR-v01.Rmd" 2>&1) || echo "Can't rename SAR v01: $error"
+
 echo "Remotes available for updating/renaming:"
 for REMOTE in $(git remote)
 do
