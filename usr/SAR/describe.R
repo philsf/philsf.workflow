7572dcfb2891a647793a5bf8fd8ba5d24d9a5600
Felipe Figueiredo <philsf79@gmail.com>
2023-01-13 17:33:12 -0300

    update gtsummary to new version

--- usr/SAR/describe.R
+++ usr/SAR/describe.R
@@ -35,4 +35,4 @@ tab_desc <- analytical %>%
   # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
   # modify_header(label ~ "**Características dos pacientes**") %>%
   # bold_labels() %>%
-  modify_table_styling(columns = "label", align = "c")
+  modify_table_styling(columns = "label", align = "center")
