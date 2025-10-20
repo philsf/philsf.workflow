2e44443a7157565fdc067c610ff345bd3643e381
Felipe Figueiredo <philsf79@gmail.com>
2025-10-19 23:19:15 -0300

    univariate table object

--- usr/SAR/24_output_primary_table_univar.R
+++ usr/SAR/24_output_primary_table_univar.R
@@ -1,57 +1,38 @@
-# setup -------------------------------------------------------------------
-# library(infer)
+# 24_output_primary_table_univar.R
 
-# tables ------------------------------------------------------------------
+# Preamble ----------------------------------------------------------------
+# Purpose: Create the Primary Inferential Table (Table 2) using univariate
+#          group comparison tests or effect sizes [Univariate Gig].
 
-# template p-value table
-tab_inf <- analytical %>%
+tab.primary.inf <- data.ads %>%
   tbl_summary(
     include = -id,
     by = exposure,
   ) %>%
-  # include study N
   add_overall() %>%
-  # pretty format categorical variables
-  # bold_labels() %>%
-  # bring home the bacon! (options: add_p or add_difference)
-  # add_p: quick and dirty
-  add_p(
-    # use Fisher test (defaults to chi-square)
-    test = all_categorical() ~ "fisher.test",
-    # use 3 digits in pvalue
-    pvalue_fun = function(x) style_pvalue(x, digits = 3),
-  ) %>%
-  # # diff: alternative to add_p
-  #   add_difference(
-  #     test = all_categorical() ~ "fisher.test",
-  #     # use 3 digits in pvalue
-  #     pvalue_fun = function(x) style_pvalue(x, digits = 3),
-  #     # ANCOVA
-  #     # adj.vars = c(sex, age, bmi),
-  #   ) %>%
-  # # EN
-  # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (adjusted by sex, age and BMI)") %>%
-  # # PT
-  # modify_header(estimate ~ '**Diferença ajustada**') %>%
-  # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (ajustada por sexo, idade e IMC)") %>%
-  # bold_labels() %>%
-  modify_table_styling(columns = "label", align = "center") %>%
-  # bold significant p values
-  bold_p()
 
-# Template Cohen's D table (obs: does NOT compute p)
-# tab_inf <- analytical %>%
-#   # select
-#   select(
-#     -id,
-#   ) %>%
-#   tbl_summary(
-#     by = exposure,
-#   ) %>%
-#   add_difference(
-#     test = all_continuous() ~ "cohens_d",
-#     # ANCOVA
-#     adj.vars = c(sex, age, bmi),
-#   ) %>%
-#   modify_header(estimate ~ '**d**') %>%
-#   bold_labels()
+  # --- CHOOSE ONE TEMPLATE OPTION BELOW ---
+
+  # TEMPLATE OPTION 1: Standard P-value Tests (add_p) ---------------------
+add_p(
+  # use Fisher test (defaults to chi-square)
+  test = all_categorical() ~ "fisher.test",
+  # use 3 digits in pvalue
+  pvalue_fun = function(x) style_pvalue(x, digits = 3),
+) %>%
+  bold_p() %>% # Bold significant p-values
+  # ----------------------------------------------------------------------
+
+# # TEMPLATE OPTION 2: Effect Size (Cohen's d) or Mean Difference (add_difference)
+# # Uncomment this block and comment out the add_p block above to switch
+# add_difference(
+#    test = all_continuous() ~ "cohens_d", # or t.test for mean difference
+#    # adj.vars = c(sex, age, bmi), # Uncomment for ANCOVA adjustment
+# ) %>%
+# # Use appropriate header/footnote for the chosen effect size or difference
+# modify_header(estimate ~ '**d**') %>% # Example for Cohen's d
+# # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (adjusted by sex, age and BMI)") %>%
+# bold_labels() %>%
+# ----------------------------------------------------------------------
+
+modify_table_styling(columns = "label", align = "center")
