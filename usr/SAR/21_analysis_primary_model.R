4c76f7adbb1f0434cadd4b1119c0a90e3bcbc5d7
Felipe Figueiredo <philsf79@gmail.com>
2025-10-19 22:14:03 -0300

    new 21 script refactored (unadjusted and adjusted models)

--- usr/SAR/21_analysis_primary_model.R
+++ usr/SAR/21_analysis_primary_model.R
@@ -1,20 +1,32 @@
-# setup -------------------------------------------------------------------
-# library(moderndive)
-library(broom)
-# library(emmeans)
-# library(lmerTest)
-# library(broom.mixed)
-# library(simputation)
-# library(mice)
-# library(ggfortify)
-# library(ggeffects)
-# library(car)
-# library(MASS)
-# library(rms)
-# library(performance)
+# 21_analysis_primary_model.R
 
-# raw estimate ------------------------------------------------------------
+# Preamble ----------------------------------------------------------------
+# Purpose: Fit the primary statistical model(s) and save the raw model object.
 
+# 1. Primary Model (Unadjusted) -------------------------------------------
+# Use the formula defined in 00_setup_global.R
+# This is typically the raw or unadjusted model
 
-# adjusted ----------------------------------------------------------------
+model.primary.obj.raw <- glm(
+  formula = outcome ~ exposure,
+  data = data.ads,
+  family = gaussian(link = "identity") # Customize family/link as needed
+)
 
+# 2. Primary Model (Adjusted) ---------------------------------------------
+# If an adjusted model is pre-specified in the SAP
+
+model.primary.obj.adj <- glm(
+  formula = model.primary.formula,
+  data = data.ads,
+  family = gaussian(link = "identity")
+)
+
+# NOTE: No gtsummary or plotting functions should be used here.
+# These raw objects (model.primary.raw.obj, model.primary.adj.obj)
+# will be formatted into final tables/figures in 22- and 23- scripts.
+
+# 3. Save model objects ---------------------------------------------------
+
+model.primary.obj.raw %>% write_rds("model.primary.obj.raw")
+model.primary.obj.adj %>% write_rds("model.primary.obj.adj")
