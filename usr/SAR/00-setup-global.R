8a71aa897ddaf26a9ffb0404d3ba4426d11e285e
Felipe Figueiredo <philsf79@gmail.com>
2025-10-23 16:10:51 -0300

    new header in 00- (to be used as template for others)

--- usr/SAR/00-setup-global.R
+++ usr/SAR/00-setup-global.R
@@ -1,8 +1,16 @@
-# 00-setup-global.R
-
-# Preamble ----------------------------------------------------------------
-# Purpose: Master script for setting up the environment.
-# Library Loading, Theme and Options and Global Functions and Labels
+# ============================================================
+# Script:   00_setup_global.R
+# Purpose:  Master script for setting up the environment.
+# Note:     Library Loading, Theme and Options and Global Functions and Labels
+#
+# Version:  0.xx.x
+# Author:   Felipe Figueiredo
+# Created:  2025-10-20
+#
+# ------------------------------------------------------------
+# QC Date:  YYYY-MM-DD
+# ------------------------------------------------------------
+# ============================================================
 
 # input & data ------------------------------------------------------------
 
