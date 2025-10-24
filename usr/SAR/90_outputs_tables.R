6b0a8aa8b11b039168f3c0c28b5f26b86514d52a
Felipe Figueiredo <philsf79@gmail.com>
2025-10-24 06:04:34 -0300

    fix: 90- remove invalid char (:) and reduce labels

--- usr/SAR/90_outputs_tables.R
+++ usr/SAR/90_outputs_tables.R
@@ -33,46 +33,45 @@ final.excel.list <- list()
 
 # Table 1: Baseline (Always created by 20-)
 if (exists("tab.primary.desc")) {
-  final.excel.list[["Table 1: Baseline"]] <- tab.primary.desc %>% as.data.frame()
+  final.excel.list[["T1 Baseline"]] <- tab.primary.desc %>% as.data.frame()
 }
 
 # Table 2: Primary Inference (Created by 32- or 34-)
 if (exists("tab.primary.inf")) {
-  final.excel.list[["Table 2: Primary Inference"]] <- tab.primary.inf %>% as.data.frame()
+  final.excel.list[["T2 Primary Inference"]] <- tab.primary.inf %>% as.data.frame()
 }
 
 # Table A1: Full Primary (Created by 32- in Multivariate Gig)
 if (exists("tab.primary.inf.full")) {
-  final.excel.list[["Table A1: Primary Inference (Full)"]] <- tab.primary.inf.full %>% as.data.frame()
+  final.excel.list[["TA1 Primary Inference (Full)"]] <- tab.primary.inf.full %>% as.data.frame()
 }
 
 # ----------------- SENSITIVITY TABLES (T3, A2) --------------------------
 # Created by 36_output_primary_sens_table.R
 if (exists("tab.primary.inf.sens")) {
-  final.excel.list[["Table 3: Sensitivity Inf"]] <- tab.primary.inf.sens %>% as.data.frame()
+  final.excel.list[["T3 Sensitivity Inf"]] <- tab.primary.inf.sens %>% as.data.frame()
 }
 if (exists("tab.primary.inf.full.sens")) {
-  final.excel.list[["Table A2: Full Sensitivity Inf"]] <- tab.primary.inf.full.sens %>% as.data.frame()
+  final.excel.list[["TA2 Full Sensitivity Inf"]] <- tab.primary.inf.full.sens %>% as.data.frame()
 }
 
 # ----------------- SECONDARY TABLES (T4, A3) -----------------------------
 # Created by 42_output_secondary_table.R
 if (exists("tab.secondary.inf")) {
-  final.excel.list[["Table 4: Secondary Inf"]] <- tab.secondary.inf %>% as.data.frame()
+  final.excel.list[["T4 Secondary Inf"]] <- tab.secondary.inf %>% as.data.frame()
 }
 if (exists("tab.secondary.inf.full")) {
-  final.excel.list[["Table A3: Full Secondary Inf"]] <- tab.secondary.inf.full %>% as.data.frame()
+  final.excel.list[["TA3 Full Secondary Inf"]] <- tab.secondary.inf.full %>% as.data.frame()
 }
 
 # ----------------- EXPLORATORY TABLES (T5, A4) ---------------------------
 # Created by 52_output_exploratory_table.R
 if (exists("tab.exploratory.inf")) {
-  final.excel.list[["Table 5: Exploratory Inf"]] <- tab.exploratory.inf %>% as.data.frame()
+  final.excel.list[["T5 Exploratory Inf"]] <- tab.exploratory.inf %>% as.data.frame()
 }
 if (exists("tab.exploratory.inf.full")) {
-  final.excel.list[["Table A4: Full Exploratory Inf"]] <- tab.exploratory.inf.full %>% as.data.frame()
+  final.excel.list[["TA4 Full Exploratory Inf"]] <- tab.exploratory.inf.full %>% as.data.frame()
 }
-
 
 # ----------------- FINAL SAVE CHECK --------------------------------------
 
